78bb63d7384f760cfe9f6d53d50519199e468f8e
NBTaco <menandor119@gmail.com>
2025-01-09 15:42:26 +0100

    esemenykezelo és validacio hozzáadása

--- main.js
+++ main.js
@@ -34,7 +34,6 @@ document.body.appendChild(h1)
 const table = document.createElement("table")
 document.body.appendChild(table)
 
-fejlecGeneralas()
 RenderTable(tomb)
 
 function fejlecGeneralas(){
@@ -57,6 +56,8 @@ function fejlecGeneralas(){
 }
 
 function RenderTable(array){
+    fejlecGeneralas()
+
     const tbody = document.createElement('tbody')
     table.appendChild(tbody)
     
@@ -89,5 +90,105 @@ function RenderTable(array){
             index++
         }
     }
-        
+}
+
+const form = document.getElementById('form')
+
+form.addEventListener('submit', function(e){
+e.preventDefault()
+
+const uralkodoHTML = document.getElementById('uralkodo_nev')
+const esemenyHTML = document.getElementById('esemeny1')
+const evszamHTML = document.getElementById('evszam1')
+const esemeny2HTML = document.getElementById('esemeny2')
+const evszam2HTML = document.getElementById('evszam2')
+
+const uralkodoV = uralkodoHTML.value
+const esemenyV = esemenyHTML.value
+const evszamV = evszamHTML.value
+const esemeny2V = esemeny2HTML.value
+const evszam2V = evszam2HTML.value
+
+let valid = true
+const errorszoveg = "A mező kitöltése kötelező!"
+const errorszoveg2 = "Minden eseményhez kell tartoznia évszámnak"
+const aktualis = e.currentTarget 
+const errorok = aktualis.querySelectorAll('.error') 
+
+for(const i of errorok)
+    i.innerHTML = ""
+
+
+if(!validacio(uralkodoHTML, errorszoveg))
+    valid = false
+
+if(!validacio(esemenyHTML, errorszoveg))
+    valid = false
+
+if(!validacio(evszamHTML, errorszoveg))
+    valid = false
+
+if(!validacio2(esemeny2HTML, evszam2HTML, errorszoveg2))
+    valid = false
+
+if(valid) {
+    if(esemeny2V == "" || evszam2V == "" ){
+        const ujObj = { 
+            uralkodo: uralkodoV,
+            esemeny: esemenyV,
+            evszam: evszamV,
+        }
+        tomb.push(ujObj)
+    }
+    else{
+        const ujObj = { 
+        uralkodo: uralkodoV,
+        esemeny: esemenyV,
+        evszam: evszamV,
+        esemeny2: esemeny2V,
+        evszam2: evszam2V
+    }
+    tomb.push(ujObj)
+    }
+}
+table.innerHTML = ""
+RenderTable(tomb)
+})
+
+function validacio(htmlelem, errorsz){ 
+    let valid = true 
+    if(htmlelem.value === ""){
+        const parent = htmlelem.parentElement
+        const errorhely = parent.querySelector(".error") 
+        if(errorhely != ""){
+            errorhely.innerHTML = errorsz
+        }
+        valid = false
+    }
+    return valid 
+}
+
+function validacio2(htmlelem1, htmlelem2, errorsz){ 
+    let valid = true
+    const parent1 = htmlelem1.parentElement
+    const parent2 = htmlelem2.parentElement
+
+    const errorhely1 = parent1.querySelector('.error')
+    const errorhely2 = parent2.querySelector('.error')
+
+    if(!validacio(htmlelem1, errorsz) && !validacio(htmlelem2, errorsz)){
+        errorhely1.innerHTML = ""
+        errorhely2.innerHTML = ""
+    }
+    else{
+        if(!validacio(htmlelem1, errorsz)){
+            errorhely1.innerHTML = errorsz
+            valid = false
+        }
+        if(!validacio(htmlelem2, errorsz)){
+            errorhely2.innerHTML = errorsz
+            valid = false
+        }
+    }
+    return valid
 }
